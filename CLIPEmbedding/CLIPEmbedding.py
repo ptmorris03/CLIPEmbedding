53d7904b03abe6f67c9540ce594849dcba3d0b03
Paul Morris <pmorris2012@fau.edu>
2021-11-17 12:08:51 -0500

    Update CLIPEmbedding.py

--- CLIPEmbedding/CLIPEmbedding.py
+++ CLIPEmbedding/CLIPEmbedding.py
@@ -2,7 +2,7 @@ from transformers import CLIPProcessor, CLIPModel
 import torch
 from torch import Tensor
 import numpy as np
-from PIL import Image
+from PIL.Image import Image
 
 from typing import Union, Iterable, Optional
 from pathlib import Path
@@ -14,6 +14,14 @@ class CLIPEmbedding:
         model_path: Union[str, Path] = "openai/clip-vit-base-patch32",
         device: Union[str, torch.device] = torch.device('cpu')
         ):
+        """
+        Arguments:
+        ----------
+        model_path: 🤗 Transformers URI or Path to checkpoint directory.
+        device: 'cpu' or 'cuda'
+        
+        See https://huggingface.co/transformers/model_doc/clip.html
+        """
         if type(device) == str:
             device = torch.device(device)
 
@@ -25,10 +33,16 @@ class CLIPEmbedding:
         self.processor = CLIPProcessor.from_pretrained(model_path)
 
 
-    def embed_images(
-        self,
-        images: Iterable[Union[Image.Image, np.array, Tensor]]
-        ):
+    def embed_images(self, images: Iterable[Union[Image, np.array, Tensor]]):
+        """
+        Arguments:
+        ----------
+        images: List of images (PIL, Numpy, or Pytorch)
+        
+        Returns:
+        --------
+        outputs: torch.Tensor of shape (n_images, 512)
+        """
         inputs = self.processor(
             images=images,
             return_tensors="pt",
@@ -44,10 +58,16 @@ class CLIPEmbedding:
         return outputs
 
 
-    def embed_text(
-        self,
-        text: Iterable[Union[Image.Image, np.array, Tensor]]
-        ):
+    def embed_text(self, text: Iterable[str]):
+        """
+        Arguments:
+        ----------
+        text: List of strings
+        
+        Returns:
+        --------
+        outputs: torch.Tensor of shape (n_text, 512)
+        """
         inputs = self.processor(
             text=text,
             return_tensors="pt",
@@ -64,10 +84,22 @@ class CLIPEmbedding:
 
 
     def similarity(self, images: Tensor, text: Tensor):
+        """
+        Arguments:
+        ----------
+        images: torch.Tensor of shape (n_images, n_dims)
+        text: torch.Tensor of shape (n_text, n_dims)
+        
+        Returns:
+        --------
+        probability: torch.Tensor of shape (n_images, n_text). Rows sum to 1.
+        """
         images = images / images.norm(dim=-1, keepdim=True)
         text = text / text.norm(dim=-1, keepdim=True)
 
         logit_scale = self.model.logit_scale.exp()
         logits = logit_scale * images @ text.t()
-        return torch.softmax(logits, dim=-1)
+        probability = torch.softmax(logits, dim=-1)
+        
+        return probability
       
